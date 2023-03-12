a938f54120aa5ee03d79aa440d18a8e605556e1a
Reynaldi Jonathan Hartono <85833200+ReynaldiJonathanHartono@users.noreply.github.com>
2023-03-12 09:47:42 +0700

    Update favRestoIdbSpec.js

--- specs/favRestoIdbSpec.js
+++ specs/favRestoIdbSpec.js
@@ -2,7 +2,7 @@
 import { itActsAsFavoriteRestoModel } from './contract/favRestoContract';
 import FavoriteRestoIdb from '../src/scripts/data/favorite-resto-idb';
 
-describe('Favorite Movie Idb Contract Test Implementation', () => {
+describe('Favorite Resto Idb Contract Test Implementation', () => {
   afterEach(async () => {
     (await FavoriteRestoIdb.getAllRestos()).forEach(async (resto) => {
       await FavoriteRestoIdb.deleteResto(resto.id);
